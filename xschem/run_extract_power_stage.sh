10b6086af0be24006b4b7bb834d9e6e27c67c402
R. Timothy Edwards <tim@opencircuitdesign.com>
2026-03-27 20:26:09 -0400

    Simple update to the xschem extraction script to make it more general-purpose so it's easier to copy and modify for other blocks or other projects.

--- xschem/run_extract_power_stage.sh
+++ xschem/run_extract_power_stage.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
+mkdir -p ../netlist/schematic
+
+project=power_stage
 
 echo ${PDK_ROOT:=/home/tim/gits} > /dev/null
 echo ${PDK:=ihp-sg13cmos5l} > /dev/null
 
-xschem -n -s -r -x -q --tcl "set top_is_subckt 1" --rcfile $PDK_ROOT/$PDK/libs.tech/xschem/xschemrc -o ../netlist/schematic -N power_stage.spice power_stage.sch
+xschem -n -s -r -x -q --tcl "set top_is_subckt 1" --rcfile $PDK_ROOT/$PDK/libs.tech/xschem/xschemrc -o ../netlist/schematic -N $project.spice $project.sch
 
+echo "Done!"
+exit 0
